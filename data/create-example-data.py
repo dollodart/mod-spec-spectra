bb6888e8aeeeddd4712b1a90414547b4c0889bb8
david <davidollodart@gmail.com>
2020-09-18 16:19:45 -0700

    add noise and phase shift to example data
    random noise is added to the example data so the period averaged and
    raw data will differ
    
    phase shift is added to allow the different transient responses to
    possibly be found by SVD, however, still only two components with
    non-physical spectra are found by the SVD

--- data/create-example-data.py
+++ data/create-example-data.py
@@ -4,10 +4,16 @@ mod_freq = 1/mod_per
 t = np.arange(0,5*mod_per,1) # time range, scan rate is 1 per second
 ang_mod_freq = mod_freq*2*np.pi
 
-cA = 1.5 + np.sin(ang_mod_freq*t)
-cAs = cA
-cBs = cAs**2
-cB = cBs
+cA = 1.5 + np.sin(ang_mod_freq*t) + \
+        np.random.random(size=len(t))/10.
+cAs = 0.1 + np.sin(ang_mod_freq*t + \
+        np.pi/6)/15. + \
+        np.random.random(size=len(t))/100.
+cBs = 0.1 + \
+        np.sin(ang_mod_freq*t + np.pi/4)/15. + \
+        np.random.random(size=len(t))/100.
+cB = 1.5 + np.sin(ang_mod_freq*t + np.pi/3) + \
+        np.random.random(size=len(t))/10.
 
 x = np.arange(1500,4000,1) # wavenumber range
 sA = np.exp(-(1800-x)**2/10) + np.exp(-(3200-x)**2/40)
